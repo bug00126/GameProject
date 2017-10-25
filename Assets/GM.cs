5bf29cfb12c22e3e5250f272e2cfbe466d47f71c
bug00126 <daniel860126@gmail.com>
2017-10-25 15:43:06 +0800

    first time to send

---
+++ Assets/GM.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+
+	public List<Player> allPlayer = new List<Player> ();
+
+	public void Login(Player player) {
+		allPlayer.Add(player);
+		player.RpcSetPlayer(allPlayer.Count);
+	}
+}
